7d105be8e8f06acbb8c18990979bf8074bd95d2e
rub3n_lh@yahoo.es <rub3n_lh@yahoo.es>
2023-09-25 10:19:07 -0400

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE1ODAz/spectral.js?branch_slug=master&token=70603d3a-7aea-40ce-8c96-a519ba0d3152";
+export default { extends: ruleset };
